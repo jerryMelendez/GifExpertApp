a5bc0973cb93fc4d3b7112b15683ee62170425bc
jerrymelendez <mjerrymoises@gmail.com>
2023-11-21 17:27:30 -0600

    antes del test configuration

---
+++ test/components/GifItem.test.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+
+describe('Pruebas en el componente', () => {
+    test('debe de hacer match con el snapshot', () => {
+        expect(true).toBe(true);
+        
+    })
+});
